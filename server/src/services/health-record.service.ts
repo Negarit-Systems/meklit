c8ae36fda2290ff89e3acb29761e94e5a4df2d97
Hermon <hermongetachew10@gmail.com>
2025-09-09 18:05:06 +0300

    fix: relocate entity-crud service from utils to services

--- server/src/services/health-record.service.ts
+++ server/src/services/health-record.service.ts
@@ -1,5 +1,5 @@
 import { HealthRecordEntry } from '../models/health-record.model.js';
-import { EntityCrudService } from '../utils/entity-crud.service.js';
+import { EntityCrudService } from './entity-crud.service.js';
 
 export class HealthRecordService extends EntityCrudService<HealthRecordEntry> {
   constructor() {
