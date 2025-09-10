e850aa55876530c11c3f990922ed2910414beac0
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2025-09-10 14:31:41 +0300

    layout modification

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -4,19 +4,29 @@ import { PublicLayout } from "./components/public-layout/PublicLayout";
 import Dashboard from "./pages/main-dashboard-page/Dashboard";
 import Comparision from "./pages/comparision-page/Comparision";
 import ChildDetail from "./pages/child-detail-page/ChildDetail";
+import Signin from "./pages/auth-pages/Signin";
+import Signup from "./pages/auth-pages/Signup";
 
 
 export const router = createBrowserRouter([
-{
-    path: "",
-    element: <PublicLayout/>,
-    children: [
-      { index: true, element: <Navigate to="/dashboard" replace /> },
-      { path: "/dashboard", element: <Dashboard /> },
-      { path: "/comparision", element: <Comparision /> },
-      { path: "/child-detail/:id", element: <ChildDetail /> },
-      { path: "*", element: <PublicNotFound /> },
-   ],
- },
+  {
+    path: "/signin",
+    element: <Signin/>,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+      path: "",
+      element: <PublicLayout/>,
+      children: [
+        { index: true, element: <Navigate to="/dashboard" replace /> },
+        { path: "/dashboard", element: <Dashboard /> },
+        { path: "/comparision", element: <Comparision /> },
+        { path: "/child-detail/:id", element: <ChildDetail /> },
+        { path: "*", element: <PublicNotFound /> },
+    ],
+  },
 ]);
 
