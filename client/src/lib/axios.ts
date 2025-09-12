36ce251161f9f537f85dcafe1f7a1613f2b6f6eb
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2025-09-12 13:43:22 +0300

    base url modification

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Set your API base URL here
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "http://localhost:5000/api/v1";
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
