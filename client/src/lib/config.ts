2d8842373c517edb012889e7b85bbdc6e9ce77a8
Hermon Getachew <78665128+Mercury1565@users.noreply.github.com>
2025-09-12 23:27:33 +0300

    Update config.ts

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -1,5 +1,5 @@
 export const config = () => {
   return {
-    BASE_URL: import.meta.env.VITE_API_URL || "https://meklit-api.negaritsystems.com.et",
+    BASE_URL: import.meta.env.VITE_API_URL || "https://meklit-api.negaritsystems.com.et/api",
   };
 };
