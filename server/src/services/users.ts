9cc9221c81c5de9521bd7566d9b82a6eed25e694
Yonatan Tizazu <yonatantizazu12@gmail.com>
2025-09-13 12:14:40 +0300

    feat: refactor auth controller

--- server/src/services/users.ts
+++
@@ -1,8 +0,0 @@
-import { Users } from '../models/users.js';
-import { EntityCrudService } from './entity-crud.js';
-
-export class UsersService extends EntityCrudService<Users> {
-  constructor() {
-    super('users');
-  }
-}
